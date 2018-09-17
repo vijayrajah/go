98709a12408719b0fb304ebfd7f46d7d39f6fcaa
vijay <me@rvijay.in>
2018-09-17 10:25:42 +0530

    Add structs2... Append structs

--- structs2/structs.go
+++ structs2/structs.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+//GLOBAL VARIABLE
+var count int
+
+type s1 struct {
+	ident   int
+	name    string
+	age     int
+	address string
+}
+
+func printAge(s1 s1) {
+	fmt.Println("Age is ", s1.age)
+}
 
 func main() {
 
-	fmt.Println("Hello go")
+	count++
+	persona := s1{ident: count, name: "Vijay", age: 35, address: "Chennai, India"}
+	printAge(persona)
+
+	personarr := make([]s1, 0)
+	for i := 0; i < 10; i++ {
+		count++
+		personarr = append(personarr, s1{ident: count, name: "Vijay" + strconv.Itoa(i), age: 35 + i, address: "Chennai, India"})
+	}
+
+	fmt.Println(personarr)
+
 }
