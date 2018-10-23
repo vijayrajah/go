2311df1fd11e088125f8b4f9308ff063801abd39
Vijay Rajah <vijay.rajah.ap@nielsen.com>
2018-10-23 22:40:37 +0530

    Add details on writting to sync channel

--- concurrency/concur.go
+++ concurrency/concur.go
@@ -24,9 +24,15 @@ func funcc(inp <-chan string) {
 func main() {
 
 	funccInp := make(chan string)
+	//funccInp <- "from main" //This will block and you will get "fatal error: all goroutines are asleep - deadlock!"
+	//This is because the channel defined is a syncronous channel, and hence, it will be blocked untill it read from
+	//Hence, the main thread is blocked and go detecs it and throws the error
+
+	// we can however, write to the channel in async way like....
 	go func() {
 		funccInp <- "from main"
 	}()
+	//This does not block the main thread, as a seperate go routine is used..
 
 	go funcc(funccInp)
 	go funcb()
